fc90cbb486664bc97b868147af0d5174feb59254
Samuel Rivera <samuelp1errot@gmail.com>
2024-03-26 19:04:00 +0500

    Added vue component

---
+++ src/components/Counter.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const counter = ref(0);
+</script>
+
+<template>
+  <button @click="counter++">Increment</button>
+  &nbsp;
+  <span>{{ counter }}</span>
+</template>
